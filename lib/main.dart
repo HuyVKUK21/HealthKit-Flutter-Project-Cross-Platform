5ceeb93d8aa2a80e52f89bb0294df1bf26c518e4
Redmi789 <98439293+Redmi789@users.noreply.github.com>
2024-11-03 21:16:14 +0700

    release: podfile runner

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,20 @@
 import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:fitnessapp/firebase_options.dart';
 import 'package:fitnessapp/presentation/bloc/signin/signin_bloc.dart';
 import 'package:fitnessapp/presentation/bloc/signup/signup_bloc.dart';
 import 'package:fitnessapp/presentation/screens/signin/signin_screen.dart';
 import 'package:fitnessapp/routes.dart';
 import 'package:fitnessapp/service_locator.dart';
-// import 'package:fitnessapp/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   
   WidgetsFlutterBinding.ensureInitialized();
-  Platform.isAndroid ? await Firebase.initializeApp(
-    options: const FirebaseOptions(
-        apiKey: "AIzaSyBylDdBXP6U5Ba6MqX8dUrZ4Q-N9nR-164",
-        appId: "1:539525530321:android:80fdb2100f11d2c57abbf0",
-        messagingSenderId: "539525530321",
-        projectId: "healthkit-flutter"
-    )) : await Firebase.initializeApp();
-  configureDependencies();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
